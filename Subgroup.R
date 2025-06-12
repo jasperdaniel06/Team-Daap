0907bbfd93a5647609d77ae1f271b86b5ef1759d
tjdykstra <joeldij@outlook.com>
2025-06-12 15:29:47 +0200

    made a new plot

--- Subgroup.R
+++ Subgroup.R
@@ -4,24 +4,7 @@ buzyness$Regio.s <- gsub('\\(PV\\)', '', buzyness$Regio.s)
 library(dplyr)
 
 buzyness <- buzyness %>%
-  mutate(Regio.s = ifelse(Regio.s == "Fryslân", "Friesland", Regio.s))
-
-
-# Check column names
-colnames(buzyness)
-
-# Look at some unique region names
-unique(buzyness$Region.s)
-
-
-library(dplyr)
-
-buzyness <- buzyness %>%
   mutate(Regio.s = if_else(Regio.s == "Fryslân ", "Friesland ", Regio.s))
-
-unique(buzyness$Regio.s)
-
-library(dplyr)
 
 buzyness <- buzyness %>%
   rename(Bedrijfstak = "Bedrijfstakken.branches..SBI.2008.")
@@ -40,25 +23,70 @@ unique(data2$Regio.s)
 
 library(ggplot2)
 library(tidyr)
+library(readr)
+library(sf)
 
-library(ggplot2)
-library(tidyr)
+
+# Load the CSV
+df_2023 <- read_csv2("bedrijvenNL2023.csv")
+df_2019 <- read_csv2("bedrijvenNL2019.csv")
+
+df_2023 <- as.data.frame(df_2023)
+df_2019 <- as.data.frame(df_2019)
+
+
+
+df_2023 <- df_2023[-c(13, 14), ]
+df_2019 <- df_2019[-c(13, 14), ]
+
+
+colnames(df_2023)[3] <- "Regio.s"
+df_2023$Regio.s <- gsub(" \\(PV\\)", "", df_2023$Regio.s)
+row.names(df_2023) <- df_2023$Regio.s
+df_2023$Regio.s <- gsub("Fryslân", "Friesland", df_2023$Regio.s)
+
+colnames(df_2019)[3] <- "Regio.s"
+df_2019$Regio.s <- gsub(" \\(PV\\)", "", df_2019$Regio.s)
+row.names(df_2019) <- df_2019$Regio.s
+df_2019$Regio.s <- gsub("Fryslân", "Friesland", df_2019$Regio.s)
+
+
+
+df_2019 <- inner_join(df_2023, df_2019, by = 'Regio.s')
+
+busi <- c() # give variable busi the value of a vector
+
+for(i in 1:nrow(df_2019))
+{
+  busi[i] = df_2019$"Vestigingen (Aantal).x"[i] / df_2019$"Vestigingen (Aantal).y"[i] # Element division  
+}
+
+df_2019 <- cbind(df_2019, busi) # add the dif variable to the data2 dataset
+
+
+
+view(df_2023)
+view(df_2019)
+
+data2 <- inner_join(df_2019, data2, by = 'Regio.s')
+
+
 
 # Create the clean dataframe from your dataset
 df <- data.frame(
   Province = data2$Regio.s,
-  New_Companies = data2$`Nieuwe bedrijven`,
-  WOZ_2020 = data2$`2020`
+  Growth_Rate_New_Companies = data2$`busi`,
+  WOZ_change = data2$`dif`
 )
 
 # Pivot to long format
-df_long <- pivot_longer(df, cols = c(New_Companies, WOZ_2020),
+df_long <- pivot_longer(df, cols = c(Growth_Rate_New_Companies, WOZ_change),
                         names_to = "Type", values_to = "Value")
 
 # Plot: side-by-side bars per province
 ggplot(df_long, aes(x = Province, y = Value, fill = Type)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "New Companies and WOZ Value per Province (2020)",
+  labs(title = "Growth of new companies and change in WOZ value per province 2019-2023",
        x = "Province", y = "Value") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
