d18b3b09aefb0eb426cb71b202ed7a0cf35fc01e
giovanni200806 <gi_ko20@outlook.com>
2025-06-12 16:06:42 +0200

    extra map plot of the houseprices in 2023

--- Spatial.R
+++ Spatial.R
@@ -25,4 +25,11 @@ ggplot(data2) +
   theme_minimal() +
   labs(title = "Map of House prices per region", fill = "House prices in 2020\nx1000 in euros")
 
+# Create the map using ggplot2
+ggplot(data2) +
+  geom_sf(aes(fill = `2023`)) +  # Mapping values from the 2020 column
+  scale_fill_gradient(low = "blue", high = "red") +
+  theme_minimal() +
+  labs(title = "Map of House prices per region", fill = "House prices in 2023\nx1000 in euros")
+
 
